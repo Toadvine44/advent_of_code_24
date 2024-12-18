0020e731a75272078a876326fe485ea7793a693a
Harry Witt <harrison.witt@gmail.com>
2024-12-18 00:48:55 -0600

    cleaned up day4 p2

--- src/day4/p2/day4_p2.go
+++ src/day4/p2/day4_p2.go
@@ -6,20 +6,6 @@ import (
 	"os"
 )
 
-func print2dSlice(mtx [][]byte) {
-	fmt.Println("================= PRINTING 2D Slice =================")
-	fmt.Println("BYTE REPRESENTATION")
-	for _, row := range mtx {
-		fmt.Println(row)
-	}
-	fmt.Println("======================================================")
-	fmt.Println("STRING REPRESENTATION")
-	for _, row := range mtx {
-		fmt.Println(string(row))
-	}
-	fmt.Println("=======================================================")
-}
-
 func isMatch(xmasMtx [][]byte, i int, j int, char byte) bool {
 	if i < 0 || j < 0 || i > len(xmasMtx)-1 || j > len(xmasMtx[i])-1 {
 		return false
@@ -29,62 +15,6 @@ func isMatch(xmasMtx [][]byte, i int, j int, char byte) bool {
 	}
 	return false
 }
-
-// func searchEast(xmasMtx [][]byte, i int, j int) bool {
-// 	return (isMatch(xmasMtx, i, j, 'X') &&
-// 		isMatch(xmasMtx, i, j+1, 'M') &&
-// 		isMatch(xmasMtx, i, j+2, 'A') &&
-// 		isMatch(xmasMtx, i, j+3, 'S'))
-// }
-//
-// func searchWest(xmasMtx [][]byte, i int, j int) bool {
-// 	return (isMatch(xmasMtx, i, j, 'X') &&
-// 		isMatch(xmasMtx, i, j-1, 'M') &&
-// 		isMatch(xmasMtx, i, j-2, 'A') &&
-// 		isMatch(xmasMtx, i, j-3, 'S'))
-// }
-//
-// func searchNorth(xmasMtx [][]byte, i int, j int) bool {
-// 	return (isMatch(xmasMtx, i, j, 'X') &&
-// 		isMatch(xmasMtx, i-1, j, 'M') &&
-// 		isMatch(xmasMtx, i-2, j, 'A') &&
-// 		isMatch(xmasMtx, i-3, j, 'S'))
-// }
-//
-// func searchSouth(xmasMtx [][]byte, i int, j int) bool {
-// 	return (isMatch(xmasMtx, i, j, 'X') &&
-// 		isMatch(xmasMtx, i+1, j, 'M') &&
-// 		isMatch(xmasMtx, i+2, j, 'A') &&
-// 		isMatch(xmasMtx, i+3, j, 'S'))
-// }
-//
-// func searchNorthEast(xmasMtx [][]byte, i int, j int) bool {
-// 	return (isMatch(xmasMtx, i, j, 'X') &&
-// 		isMatch(xmasMtx, i-1, j+1, 'M') &&
-// 		isMatch(xmasMtx, i-2, j+2, 'A') &&
-// 		isMatch(xmasMtx, i-3, j+3, 'S'))
-// }
-//
-// func searchNorthWest(xmasMtx [][]byte, i int, j int) bool {
-// 	return (isMatch(xmasMtx, i, j, 'X') &&
-// 		isMatch(xmasMtx, i-1, j-1, 'M') &&
-// 		isMatch(xmasMtx, i-2, j-2, 'A') &&
-// 		isMatch(xmasMtx, i-3, j-3, 'S'))
-// }
-//
-// func searchSouthEast(xmasMtx [][]byte, i int, j int) bool {
-// 	return (isMatch(xmasMtx, i, j, 'X') &&
-// 		isMatch(xmasMtx, i+1, j+1, 'M') &&
-// 		isMatch(xmasMtx, i+2, j+2, 'A') &&
-// 		isMatch(xmasMtx, i+3, j+3, 'S'))
-// }
-//
-// func searchSouthWest(xmasMtx [][]byte, i int, j int) bool {
-// 	return (isMatch(xmasMtx, i, j, 'X') &&
-// 		isMatch(xmasMtx, i+1, j-1, 'M') &&
-// 		isMatch(xmasMtx, i+2, j-2, 'A') &&
-// 		isMatch(xmasMtx, i+3, j-3, 'S'))
-// }
 
 func checkDiag1(xmasMtx [][]byte, i int, j int) bool {
 
@@ -115,32 +45,6 @@ func xmasSearch(xmasMtx [][]byte, i int, j int) bool {
 	if checkDiag1(xmasMtx, i, j) && checkDiag2(xmasMtx, i, j) {
 		return true
 	}
-
-	// if searchEast(xmasMtx, i, j) {
-	// 	cnt++
-	// }
-	// if searchWest(xmasMtx, i, j) {
-	// 	cnt++
-	// }
-	// if searchNorth(xmasMtx, i, j) {
-	// 	cnt++
-	// }
-	// if searchSouth(xmasMtx, i, j) {
-	// 	cnt++
-	// }
-	// if searchNorthEast(xmasMtx, i, j) {
-	// 	cnt++
-	// }
-	// if searchNorthWest(xmasMtx, i, j) {
-	// 	cnt++
-	// }
-	// if searchSouthEast(xmasMtx, i, j) {
-	// 	cnt++
-	// }
-	// if searchSouthWest(xmasMtx, i, j) {
-	// 	cnt++
-	// }
-
 	return false
 }
 
